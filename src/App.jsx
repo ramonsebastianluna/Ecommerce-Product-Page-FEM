25c98c749e81cb0cbe106e5fe05ba97dc2a0fcef
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-10-09 18:00:23 -0300

    chore(elimination): deleted default styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Site under construction...</h1>
-      <p className="read-the-docs">
-        You will be able to see this project very soon.
-      </p>
+      <div>mi App</div>
     </>
   )
 }
